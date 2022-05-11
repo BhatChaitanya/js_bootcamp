fb562d74aeab62577b35c5be81f716f548301d26
BhatChaitanya <chaitanya.bhat@freightdeck.com>
2022-05-11 07:51:49 +0530

    first commit

---
+++ index_practice.js
@@ -0,0 +1,11 @@
+alert("i'm Javascript")
+var lion = {
+    name:"simbha",
+    age:4
+}
+document.write(lion.age);
+var proper = 'name';
+document.write(lion[proper]);
+
+var things = ['hello', 'world', 15, $];
+console.log(things.length);
